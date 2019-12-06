9c8a11d1c4db92796caa59553f1c2e4d7732b207
Sebastian Landwehr <info@dword-design.de>
2019-12-06 19:08:39 +0100

    fix base upgrade

--- test/start.test.js
+++ test/start.test.js
@@ -1,31 +1,31 @@
 import { spawn } from 'child_process'
 import withLocalTmpDir from 'with-local-tmp-dir'
 import expect from 'expect'
-import waitForChange from 'wait-for-change'
-import importFresh from 'import-fresh'
 import { resolve, join } from 'path'
 import outputFiles from 'output-files'
 import { outputFile, readFile } from 'fs'
-import resolveBin from 'resolve-bin'
 import { endent } from '@functions'
-import delay from 'delay'
 import { minimalProjectConfig } from '@dword-design/base'
+import waitOn from 'wait-on'
 
 export const it = () => withLocalTmpDir(__dirname, async () => {
   await outputFiles({
     ...minimalProjectConfig,
-    'src/index.js': 'export default 1',
+    'src/index.js': endent`
+      import { outputFile } from 'fs'
+
+      export default () => outputFile('foo.txt', 1)
+    `,
     'src/cli.js': endent`
       #!/usr/bin/env node
 
       import api from '.'
-      import { outputFile } from 'fs'
 
-      outputFile('foo.txt', api)
+      api()
     `,
   })
 
-  const childProcess = await spawn(resolveBin.sync('@dword-design/base-server', { executable: 'base-server' }), ['start'])
+  const childProcess = await spawn('base-server', ['start'])
     .catch(error => {
       if (error.code !== null) {
         throw error
@@ -33,14 +33,16 @@ export const it = () => withLocalTmpDir(__dirname, async () => {
     })
     .childProcess
   try {
-    await waitForChange(join('dist', 'index.js'))
-    expect(require(resolve('dist'))).toEqual(1)
-    await delay(700)
+    await waitOn({ resources: [join('dist', 'index.js'), 'foo.txt'] })
+    expect(typeof require(resolve('dist'))).toEqual('function')
     expect(await readFile('foo.txt', 'utf8')).toEqual('1')
-    await outputFile(join('src', 'index.js'), 'export default 2')
-    await waitForChange(join('dist', 'index.js'))
-    expect(importFresh(resolve('dist'))).toEqual(2)
-    await delay(700)
+    await outputFile(join('src', 'index.js'), endent`
+      import { outputFile } from 'fs'
+
+      export default () => outputFile('foo.txt', 2)
+    `)
+    await waitOn({ resources: [join('dist', 'index.js'), 'foo.txt'] })
+    expect(typeof require(resolve('dist'))).toEqual('function')
     expect(await readFile('foo.txt', 'utf8')).toEqual('2')
   } finally {
     childProcess.kill()
